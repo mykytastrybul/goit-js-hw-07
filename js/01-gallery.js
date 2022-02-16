5e75553e3c324c962c78eff23bbc69138f60fae3
s3bull <95282477+s3bull@users.noreply.github.com>
2022-02-16 19:31:00 +0200

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = (items) => {
+  return items.map(({ preview, original, description }) => `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`).join("");
+}
+gallery.innerHTML = galleryMarkup(galleryItems);
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  event.target.classList.contains("gallery__image");
+  openModal(event);
+})
+const openModal = (event) => {
+  const origImg = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${origImg}">`, {
+    onShow: () => {
+      window.addEventListener("keydown", closeModal);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", closeModal);
+    }
+  })
+  const closeModal = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = (items) => {
+  return items.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`).join("");
+}
+gallery.innerHTML = galleryMarkup(galleryItems);
+let lightbox = new SimpleLightbox(".gallery a",{ captionsData: "alt" , captionDelay: 250 });
